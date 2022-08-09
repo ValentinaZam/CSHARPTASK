3e149213d1717486d3156970b16f08fe87aa3d1d
Валентина <abc99-33@yandex.ru>
2022-08-10 01:39:08 +0300

    Task3-7

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите число, которе на вход принимает одно сило N, 
+// а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+Console.WriteLine("Промежуток:  ");
+for (int i = - num; i <= num ; i++ )
+
+{
+      Console.Write(i + " ");
+}
