55c3764d64f839cde9fafb4b3ce0f455d57216bb
Валентина <abc99-33@yandex.ru>
2022-08-10 00:56:41 +0300

    Task1

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -8,3 +8,5 @@ Console.WriteLine("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int square = num * num;
 Console.WriteLine($"Квадрат числа {square} ");
+
+//Задача решена
--- Tasr1/Program.cs
+++ Tasr1/Program.cs
@@ -1,13 +1,21 @@
-﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя).
-// Напромер:
-//   4-> 16
-//   -3 -> 9
-//   -7 -> 49
+﻿// Напишите программу, которая на вход принимает два числа и проверяет
+// Является ли первое число квадратом второго
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3, b = 9 -> нет
 
-Console.WriteLine("Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int square = num * num;
-Console.WriteLine($"Квадрат числа {square} ");
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 * num2 == num1)
+Console.WriteLine($"Число {num1} является квадратом числа {num2} ");
+else 
+Console.WriteLine($"Число {num1} НЕ является квадратом числа {num2} ");
 
 
 
