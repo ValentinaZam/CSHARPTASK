37d8590b85e4b30db896eaa29f7eeb2f1c51d201
Валентина <abc99-33@yandex.ru>
2022-08-10 21:33:37 +0300

    Task10-15

---
+++ Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет. Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num > 99)
+// {
+// //  Console.WriteLine(num.ToString()[2]);
+//     for (int i = 0; i < num.Length; i++)
+//     {
+//       int a = num / 10;
+//       Console.WriteLine($"{a}");
+//     }
+    
+// }
+// else Console.WriteLine("Третьей цифры нет");
+
+
+if (num > 99)
+{
+    while (num > 999)
+    {  
+        num = num / 10;
+    }
+  Console.WriteLine(num % 10);
+}
+else Console.WriteLine("Третьей цифры нет");
+
+
---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1-7: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+if (num == 6 || num == 7)
+   Console.WriteLine("Да - это ВЫХОДНОЙ!!!");
+ else if (num >= 1 && num <= 5)   Console.WriteLine("Это будний день");
+
+else Console.WriteLine("Число не в заданном диапозоне"); 
